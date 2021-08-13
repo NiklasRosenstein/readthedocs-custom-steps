719ee46ed4e34a869461799c9d018bd934447b6a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-13 19:01:59 +0200

    update test to also check sphinx/mkdocs --version output

--- src/test/test_readthedocs_custom_steps.py
+++ src/test/test_readthedocs_custom_steps.py
@@ -1,11 +1,33 @@
 
+import contextlib
+import re
+import shlex
 import textwrap
+import tempfile
+import typing as t
 from pathlib import Path
 import docker  # type: ignore
 import pytest
 
 PIP_CACHES_VOLUME = 'pip-caches'
 PROJECT_DIRECTORY = str(Path(__file__).parent.parent.parent.resolve())
+CUSTOM_STEPS_YAML = '''
+steps:
+- echo rtd-custom-steps says "$@"
+- python -m "${1}" --version
+'''
+
+
+def stop_and_remove_container(container):
+  try:
+    container.reload()
+    if container.status == 'running':
+      container.stop()
+    container.reload()
+    if container.status == 'running':
+      container.kill()
+  finally:
+    container.remove()
 
 
 @pytest.mark.parametrize(
@@ -17,35 +39,52 @@ PROJECT_DIRECTORY = str(Path(__file__).parent.parent.parent.resolve())
     'python:3.9',
   ])
 @pytest.mark.parametrize(
-  argnames='command',
+  argnames=('command', 'requirements', 'second_line_regex'),
   argvalues=[
-    'mkdocs build --clean --site-dir _site/html --config-file mkdocs.yml',
-    'sphinx -T -b html -d _build/doctrees -D language=en . _build/html',
+    ('mkdocs build --clean --site-dir _site/html --config-file mkdocs.yml', ['mkdocs'], r'python -m mkdocs, version .*$'),
+    ('sphinx -T -b html -d _build/doctrees -D language=en . _build/html', ['sphinx'], r'(__main__\.py|sphinx) .*$'),
   ])
-def test_mkdocs_hook(python_image: str, command: str) -> None:
+def test_mkdocs_hook(python_image: str, command: str, requirements: t.List[str], second_line_regex: str) -> None:
+  """
+  This unittest uses Docker to test the readthedocs-custom-steps hook, verifying the output from
+  the custom steps as well as the reentry into the hook invoking the same command but not running
+  into an infinite loop of invoking itself.
+  """
+
+
   client = docker.from_env()
-  container = client.containers.run(
-    python_image,
-    volumes={
-      PIP_CACHES_VOLUME: {'bind': '/root/.cache/pip', 'mode': 'rw'},
-      PROJECT_DIRECTORY: {'bind': '/opt/project', 'mode': 'ro'}},
-    command=['bash', '-c', textwrap.dedent(f'''
-      set -e
-      cp -r /opt/project /tmp/rtdcs
-      rm -rf /tmp/rtdcs/*.egg-info /tmp/rtdcs/build
-      READTHEDOCS=True pip install -q /tmp/rtdcs 2>/dev/null
-      mkdir /tmp/test; cd /tmp/test; cp /opt/project/src/test/.readthedocs-custom-steps.yml .
-      python -m {command}
-    ''')],
-    detach=True,
-    stdin_open=False,
-  )
-  try:
+  with contextlib.ExitStack() as stack:
+    tmpfile = stack.enter_context(tempfile.NamedTemporaryFile('w', delete=False))
+    stack.callback(Path(tmpfile.name).unlink)
+    tmpfile.write(CUSTOM_STEPS_YAML)
+    tmpfile.close()
+
+    container = client.containers.run(
+      python_image,
+      volumes={
+        PIP_CACHES_VOLUME: {'bind': '/root/.cache/pip', 'mode': 'rw'},
+        PROJECT_DIRECTORY: {'bind': '/opt/project', 'mode': 'ro'},
+        tmpfile.name: {'bind': '/opt/.readthedocs-custom-steps.yml', 'mode': 'ro'}},
+      command=['bash', '-c', textwrap.dedent(f'''
+        set -e
+        cp -r /opt/project /tmp/rtdcs
+        rm -rf /tmp/rtdcs/*.egg-info /tmp/rtdcs/build
+        READTHEDOCS=True pip install -q {" ".join(map(shlex.quote, requirements))} /tmp/rtdcs 2>/dev/null
+        mkdir /tmp/test; cd /tmp/test; cp /opt/.readthedocs-custom-steps.yml .
+        python -m {command}
+      ''')],
+      detach=True,
+      stdin_open=False,
+    )
+
+    stack.callback(lambda: stop_and_remove_container(container))
+
     exit_code = container.wait()['StatusCode']
     result = container.logs().decode()
     if exit_code != 0:
       print(result)
       raise RuntimeError(f'container returned exit code {exit_code}')
-    assert result.rstrip() == f'rtd-custom-steps say {command}'
-  finally:
-    container.remove()
+
+    lines = result.splitlines()
+    assert lines[0] == f'rtd-custom-steps says {command}'
+    assert re.match(second_line_regex, lines[1]) is not None
