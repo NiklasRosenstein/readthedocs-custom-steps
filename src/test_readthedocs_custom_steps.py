5ad803e3cbe172fc53c600e09e4e20f67e40f938
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-17 01:16:58 +0200

    extend unittest

--- src/test_readthedocs_custom_steps.py
+++ src/test_readthedocs_custom_steps.py
@@ -16,6 +16,11 @@ steps:
 - echo rtd-custom-steps says "$@"
 - python -m "${1}" --version
 '''
+READTHEDOCS_CONFIG_YAML = '''
+python:
+  install:
+    - requirements: rtd/requirements.txt
+'''
 
 
 def stop_and_remove_container(container):
@@ -39,12 +44,27 @@ def stop_and_remove_container(container):
     'python:3.9',
   ])
 @pytest.mark.parametrize(
+  argnames='config_dir',
+  argvalues=['.', 'docs'],
+)
+@pytest.mark.parametrize(
+  argnames='rtd_config',
+  argvalues=[None, READTHEDOCS_CONFIG_YAML],
+)
+@pytest.mark.parametrize(
   argnames=('command', 'requirements', 'second_line_regex'),
   argvalues=[
     ('mkdocs build --clean --site-dir _site/html --config-file mkdocs.yml', ['mkdocs'], r'python -m mkdocs, version .*$'),
     ('sphinx -T -b html -d _build/doctrees -D language=en . _build/html', ['sphinx'], r'(__main__\.py|sphinx) .*$'),
   ])
-def test_mkdocs_hook(python_image: str, command: str, requirements: t.List[str], second_line_regex: str) -> None:
+def test_mkdocs_hook(
+  python_image: str,
+  config_dir: str,
+  rtd_config: t.Optional[str],
+  command: str,
+  requirements: t.List[str],
+  second_line_regex: str,
+) -> None:
   """
   This unittest uses Docker to test the readthedocs-custom-steps hook, verifying the output from
   the custom steps as well as the reentry into the hook invoking the same command but not running
@@ -59,31 +79,39 @@ def test_mkdocs_hook(python_image: str, command: str, requirements: t.List[str],
     tmpfile.write(CUSTOM_STEPS_YAML)
     tmpfile.close()
 
-    print("starting container")
+    volumes = {
+      PIP_CACHES_VOLUME: {'bind': '/root/.cache/pip', 'mode': 'rw'},
+      PROJECT_DIRECTORY: {'bind': '/opt/project', 'mode': 'ro'},
+      tmpfile.name: {'bind': '/opt/.readthedocs-custom-steps.yml', 'mode': 'ro'}}
+
+    if rtd_config:
+      tmpfile = stack.enter_context(tempfile.NamedTemporaryFile('w', delete=False))
+      stack.callback(Path(tmpfile.name).unlink)
+      tmpfile.write(rtd_config)
+      tmpfile.close()
+      volumes[tmpfile.name] = {'bind': '/opt/.readthedocs.yml', 'mode': 'ro'}
+
     container = client.containers.run(
       python_image,
-      volumes={
-        PIP_CACHES_VOLUME: {'bind': '/root/.cache/pip', 'mode': 'rw'},
-        PROJECT_DIRECTORY: {'bind': '/opt/project', 'mode': 'ro'},
-        tmpfile.name: {'bind': '/opt/.readthedocs-custom-steps.yml', 'mode': 'ro'}},
+      volumes=volumes,
       command=['bash', '-c', textwrap.dedent(f'''
         set -e
         cp -r /opt/project /tmp/rtdcs
         rm -rf /tmp/rtdcs/*.egg-info /tmp/rtdcs/build
         READTHEDOCS=True pip install -q {" ".join(map(shlex.quote, requirements))} /tmp/rtdcs 2>/dev/null
-        mkdir /tmp/test; cd /tmp/test; cp /opt/.readthedocs-custom-steps.yml .
+        mkdir /tmp/test; cd /tmp/test;
+        if [ -f /opt/.readthedocs.yml ]; then cp /opt/.readthedocs.yml .; fi
+        mkdir -p {config_dir}
+        cp /opt/.readthedocs-custom-steps.yml {config_dir}
         python -m {command}
       ''')],
-      detach=False,
+      detach=True,
       stdin_open=False,
     )
 
-    print("started container")
     stack.callback(lambda: stop_and_remove_container(container))
 
-    print("waiting container")
     exit_code = container.wait()['StatusCode']
-    print("done container", exit_code)
     result = container.logs().decode()
     if exit_code != 0:
       print(result)
